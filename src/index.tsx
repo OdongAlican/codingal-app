4c4fc4018f48495914f23f4cf5bb27d459186bd0
OdongAlican <sandieo.2020@gmail.com>
2021-06-22 15:14:54 +0300

    Add the following - Set up redux for state management - Install packages required for redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 ReactDOM.render(
   <React.StrictMode>
