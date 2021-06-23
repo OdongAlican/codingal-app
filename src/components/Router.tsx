e2f28743ac1543ee6dbf6f572094e816cf767dbe
OdongAlican <sandieo.2020@gmail.com>
2021-06-23 15:35:57 +0300

    update routes

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,24 @@
 /* eslint-disable import/extensions */
 
 import React from 'react';
+import {
+  BrowserRouter, Route, Switch,
+} from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
+import Posts from './Pages/Posts';
+import Home from './Pages/Home';
 
 const Router = () => (
   <div>
-    <Navbar />
-    <p>Router section</p>
+    <div className="router-section">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/posts" component={Posts} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   </div>
 );
 
