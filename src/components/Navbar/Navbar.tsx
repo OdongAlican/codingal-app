29f1438889c918dde422d539495a169fff8775a0
OdongAlican <sandieo.2020@gmail.com>
2021-06-23 10:56:52 +0300

    add cancelSubmit and submitForm methods

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,27 +4,37 @@ import React, { useState } from 'react';
 import './index.css';
 import { Button } from '../Generics/Generics';
 import Modal from '../Modal/Modal';
+import Timer from '../Timer/Timer';
 
 const Navbar = () => {
   const [modal, setModal] = useState<boolean>(false);
   const modalHeader = 'Select a reason to end Class';
+  const { stopWatch, watch } = Timer();
 
   const displayModal = (value: boolean): void => setModal(value);
+  const cancelSubmit = (): void => setModal(false);
+  const submitForm = (): void => {
+    stopWatch();
+    setModal(false);
+  };
 
   return (
     <div className="navbar-section">
-      {modal && (<Modal header={modalHeader} modalFunction={displayModal} />)}
+      {modal && (
+      <Modal
+        header={modalHeader}
+        submitForm={submitForm}
+        cancelSubmit={cancelSubmit}
+        modalFunction={displayModal}
+      />
+      )}
       <div className="nav-image-section" />
       <div className="horizontal-section" />
       <div className="trial-lesson-section">
         Trial Lesson [Grade 1-3]
       </div>
       <div className="timer-section">
-        34
-        {' '}
-        :
-        {' '}
-        40
+        { watch }
       </div>
       <div className="nav-button-section">
         <Button
