f6f744a096950a26fca1ef9f9eae3350f89b1d94
OdongAlican <sandieo.2020@gmail.com>
2021-06-23 10:04:53 +0300

    add ClassData component

---
+++ src/components/Modal/ClassData.ts
@@ -0,0 +1,50 @@
+/* eslint-disable import/prefer-default-export */
+
+export const ClassData = [
+  {
+    id: 1,
+    title: 'Class Completed',
+    activeIcon: 'fas fa-check-circle',
+    inActiveIcon: 'far fa-circle',
+    subDropdown: [],
+  },
+  {
+    id: 2,
+    title: 'Class interrupted/aborted',
+    activeIcon: 'fas fa-check-circle',
+    inActiveIcon: 'far fa-circle',
+    subDropdown: [
+      {
+        id: 1,
+        title: 'Student didn\'t show up for class ',
+        activeIcon: 'fas fa-check-circle',
+        inActiveIcon: 'far fa-circle',
+      },
+      {
+        id: 2,
+        title: 'Student didn\'t show any interest',
+        activeIcon: 'fas fa-check-circle',
+        inActiveIcon: 'far fa-circle',
+      },
+      {
+        id: 3,
+        title: 'Student got disconnected',
+        activeIcon: 'fas fa-check-circle',
+        inActiveIcon: 'far fa-circle',
+      },
+      {
+        id: 4,
+        title: 'I got disconnected',
+        activeIcon: 'fas fa-check-circle',
+        inActiveIcon: 'far fa-circle',
+      },
+      {
+        id: 5,
+        title: 'Other reason',
+        activeIcon: 'fas fa-check-circle',
+        inActiveIcon: 'far fa-circle',
+      },
+
+    ],
+  },
+];
