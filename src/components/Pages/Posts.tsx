a5a0fcf970c5986f373e3192a2ab5d00dba034cd
OdongAlican <sandieo.2020@gmail.com>
2021-06-23 17:39:16 +0300

    update Posts layout

--- src/components/Pages/Posts.tsx
+++ src/components/Pages/Posts.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
+/* eslint-disable no-nested-ternary */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
 import { fetchPostRequest } from '../../actions';
 import { PostData } from '../Interface/Interface';
@@ -9,19 +10,43 @@ import './index.css';
 
 const Posts = () => {
   const dispatch = useDispatch();
-  const postList = useSelector((state: RootStateOrAny) => state.postReducer.posts);
+  const postList = useSelector((state: RootStateOrAny) => state.postReducer);
+  const [pageNumber, setPageNumber] = useState<number>(1);
 
   useEffect(() => {
-    dispatch(fetchPostRequest());
-  }, []);
+    dispatch(fetchPostRequest(pageNumber));
+  }, [pageNumber]);
+
+  const scrollToEnd = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop
+      === document.documentElement.offsetHeight) {
+      scrollToEnd();
+    }
+  };
 
   return (
     <div className="display-post-list">
-      {
-        postList.map((post: PostData) => (
-          <PostDisplay key={post.id} title={post.title} body={post.body} />
-        ))
-      }
+      { Object.keys(postList.posts).length > 0 ? (
+        <div>
+          {
+            postList.posts.map((post: PostData) => (
+              <PostDisplay
+                key={post.id}
+                title={post.title}
+                body={post.body}
+                id={post.id}
+              />
+            ))
+          }
+        </div>
+      ) : postList.loading ? (<div className="loading-section">Loading !!</div>)
+        : postList.error.length > 0 ? (<div className="error-section">{postList.error}</div>)
+          : null}
     </div>
   );
 };
